17c62a3617133ee35788f11667c54972ae786e51
Fah <farhan.abuhazan@gmail.com>
2020-10-18 21:42:32 +0700

    add google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {NextPage} from 'next'
+import {useRouter} from 'next/router'
+
 import 'assets/App.scss'
 import 'assets/index.css'
 import 'react-typist/dist/Typist.css'
@@ -7,8 +9,22 @@ import 'slick-carousel/slick/slick.css'
 import 'slick-carousel/slick/slick-theme.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Layout from 'src/components/Layout'
+import * as gtag from 'src/lib/gtag'
 
 const App: NextPage<any> = ({Component, pageProps}) => {
+  
+  const router = useRouter()
+  useEffect(() => {
+    router.events.on('routeChangeComplete', (url) => {
+      gtag.pageview(url)
+    })
+    return () => {
+      router.events.on('routeChangeComplete', (url) => {
+        gtag.pageview(url)
+      })
+    }
+  }, [router.events])
+  
   return (
     <>
       <Layout title="Avepha - Home">
